d99a1622b9fef746d8cc069fd6243ae8b2413071
Joost-Jansen <joost.jjj@gmail.com>
2023-03-31 15:15:05 +0200

    Made a data diviser

---
+++ datasets/datasplitter.py
@@ -0,0 +1,25 @@
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+import os
+
+def getPercentageOfData(file, percentage):
+    df = pd.read_csv(file)
+    df = df.sample(frac=1).reset_index(drop=True)
+    df_train, df_test = train_test_split(df, test_size=percentage)
+    return df_train, df_test
+
+
+
+if __name__ == "__main__":
+    folders = ["./MQ2008/Fold1", "./MSLR-WEB10K/Fold1"]
+    files = ["test.txt", "train.txt", "vali.txt"]
+    percentages = [0.5, 0.6, 0.7, 0.8]
+
+    for folder in folders:
+        for percentage in percentages:
+            if not os.path.exists(folder+ "_percentage_" + str(percentage*100)):
+                os.mkdir(folder+ "_percentage_" + str(percentage*100))
+            for file in files:
+                df_train, df_test = getPercentageOfData(folder + "/" + file, (1-percentage))
+                df_train.to_csv(folder+ "_percentage_" + str(percentage*100) +  "/" + file , index=False)
