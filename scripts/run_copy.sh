44a96a3f0eb3f085e7b2229880fbbd8fc1e33da5
Joost-Jansen <joost.jjj@gmail.com>
2023-03-31 19:07:06 +0200

    -Made testmodel.py to test the test.txt file for each percentage of folders -also adapted run_copy.sh and test_models.sh to right format -some other small bug fixes as well

--- scripts/run_copy.sh
+++ scripts/run_copy.sh
@@ -5,15 +5,30 @@
 
 DIR=$(dirname $0)
 PROJECT_DIR="$(cd $DIR/..; pwd)"
-
+PROJECT_DIR2=/mnt/c/Users/joost/Documents/allRank
+echo $PROJECT_DIR2;
 #pointwise
-#docker run -e PYTHONPATH=/allrank -v $PROJECT_DIR:/allrank allrank:latest /bin/sh -c 'python allrank/main.py --config-file-name allrank/config0.json --run-id first_test --job-dir /allrank/first_test'
+# docker run -e PYTHONPATH=/allrank -v $PROJECT_DIR:/allrank allrank:latest /bin/sh -c 'python allrank/main.py --config-file-name allrank/config0.json --run-id first_test --job-dir results/first_test'
 
-#RankNet
-#docker run -e PYTHONPATH=/allrank -v $PROJECT_DIR:/allrank allrank:latest /bin/sh -c 'python allrank/main.py --config-file-name allrank/config_rankNet.json --run-id result_rankNet --job-dir /allrank/result_rankNet'
+# #RankNet
+# docker run -e PYTHONPATH=/allrank -v $PROJECT_DIR:/allrank allrank:latest /bin/sh -c 'python allrank/main.py --config-file-name allrank/config_rankNet.json --run-id result_rankNet --job-dir results/result_rankNet'
+docker run -e PYTHONPATH=/allrank -v $PROJECT_DIR:/allrank allrank:latest /bin/sh -c 'python allrank/main.py --config-file-name allrank/config_rankNet.json --run-id result_50 --job-dir results/result_rankNet/';
+docker run -e PYTHONPATH=/allrank -v $PROJECT_DIR:/allrank allrank:latest /bin/sh -c 'python allrank/main.py --config-file-name allrank/config_rankNet.json --run-id result_60 --job-dir results/result_rankNet/';
+docker run -e PYTHONPATH=/allrank -v $PROJECT_DIR:/allrank allrank:latest /bin/sh -c 'python allrank/main.py --config-file-name allrank/config_rankNet.json --run-id result_70 --job-dir results/result_rankNet/';
+docker run -e PYTHONPATH=/allrank -v $PROJECT_DIR:/allrank allrank:latest /bin/sh -c 'python allrank/main.py --config-file-name allrank/config_rankNet.json --run-id result_80 --job-dir results/result_rankNet/';
+echo "RankNet done";
 
-#ApproxNDCG
-#docker run -e PYTHONPATH=/allrank -v $PROJECT_DIR:/allrank allrank:latest /bin/sh -c 'python allrank/main.py --config-file-name allrank/config_ApproxNDCG.json --run-id result_ApproxNDCG --job-dir /allrank/result_ApproxNDCG'
+# #ApproxNDCG
+# docker run -e PYTHONPATH=/allrank -v $PROJECT_DIR:/allrank allrank:latest /bin/sh -c 'python allrank/main.py --config-file-name allrank/config_ApproxNDCG.json --run-id result_ApproxNDCG --job-dir results/result_ApproxNDCG'
+docker run -e PYTHONPATH=/allrank -v $PROJECT_DIR:/allrank allrank:latest /bin/sh -c 'python allrank/main.py --config-file-name allrank/config_ApproxNDCG.json --run-id result_50 --job-dir results/result_ApproxNDCG/';
+docker run -e PYTHONPATH=/allrank -v $PROJECT_DIR:/allrank allrank:latest /bin/sh -c 'python allrank/main.py --config-file-name allrank/config_ApproxNDCG.json --run-id result_60 --job-dir results/result_ApproxNDCG/';
+docker run -e PYTHONPATH=/allrank -v $PROJECT_DIR:/allrank allrank:latest /bin/sh -c 'python allrank/main.py --config-file-name allrank/config_ApproxNDCG.json --run-id result_70 --job-dir results/result_ApproxNDCG/';
+docker run -e PYTHONPATH=/allrank -v $PROJECT_DIR:/allrank allrank:latest /bin/sh -c 'python allrank/main.py --config-file-name allrank/config_ApproxNDCG.json --run-id result_80 --job-dir results/result_ApproxNDCG/';
+echo "done ApproxNDCG";
 
 #NeuralNDCG
-docker run -e PYTHONPATH=/allrank -v $PROJECT_DIR:/allrank allrank:latest /bin/sh -c 'python allrank/main.py --config-file-name allrank/config_NeuralNDCG.json --run-id result_NeuralNDCG --job-dir /allrank/result_NeuralNDCG'
+docker run -e PYTHONPATH=/allrank -v $PROJECT_DIR:/allrank allrank:latest /bin/sh -c 'python allrank/main.py --config-file-name allrank/config_NeuralNDCG.json --run-id result_50 --job-dir results/result_NeuralNDCG/';
+docker run -e PYTHONPATH=/allrank -v $PROJECT_DIR:/allrank allrank:latest /bin/sh -c 'python allrank/main.py --config-file-name allrank/config_NeuralNDCG.json --run-id result_60 --job-dir results/result_NeuralNDCG/';
+docker run -e PYTHONPATH=/allrank -v $PROJECT_DIR:/allrank allrank:latest /bin/sh -c 'python allrank/main.py --config-file-name allrank/config_NeuralNDCG.json --run-id result_70 --job-dir results/result_NeuralNDCG/';
+docker run -e PYTHONPATH=/allrank -v $PROJECT_DIR:/allrank allrank:latest /bin/sh -c 'python allrank/main.py --config-file-name allrank/config_NeuralNDCG.json --run-id result_80 --job-dir results/result_NeuralNDCG/';
+echo "done";
